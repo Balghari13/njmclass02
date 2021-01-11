95125f96e218167b4ac4f5d36c5488a3e0e5e875
Balghari <najmbalghari13@gmail.com>
2021-01-11 13:40:05 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Hello from "./Hello";
 function App({name,age}) {
   return (
     <div>
-      <h1>Hello from app.js {name} age is {(age)+10}</h1>
+      <h1>Hello from app.js {name} age is {(age)+5}</h1>
       <Hello firstName = {name} />
     </div>
   );
